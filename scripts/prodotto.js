a173a0b87697d25ddea4236638a1dd4a5fb10107
Antonio Ciusa <antonio.ciusa@mail.polimi.it>
2016-07-20 15:42:06 +0200

    prodotto

---
+++ scripts/prodotto.js
@@ -0,0 +1,35 @@
+$( document ).ready(function retrieveData() {
+    
+    
+    window.alert(window.location.href);
+    
+    var url=window.location.href;
+    window.alert("URL "+url);
+    
+    var end=url.length;
+    window.alert("END "+end);
+    
+    var start = url.lastIndexOf("=");
+    window.alert("START "+start);
+    
+    start;
+    window.alert("START++ "+start);
+    
+    var id=url.slice(start, end);
+    window.alert("ID "+id);
+    
+        
+    
+    
+    
+    
+    document.getElementById("marca").innerHTML = "Paragraph changed.";
+    document.getElementById("modello").innerHTML = "Paragraph changed.";
+    document.getElementById("descrizione").innerHTML = "Paragraph changed.";
+    document.getElementById("prezzo_listino").innerHTML = "Paragraph changed.";
+    document.getElementById("prezzo_vendita").innerHTML = "Paragraph changed.";
+    document.getElementById("prezzo_rate").innerHTML = "Paragraph changed.";
+    document.getElementById("numero_rate").innerHTML = "Paragraph changed.";
+    
+});
+
